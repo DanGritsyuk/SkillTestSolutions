e517a2dd7e0b09d811582056338515bcc76e9aa3
DanGritsyuk <denis@gritsyuk.ru>
2025-11-22 16:14:26 +0300

    D task done.

--- Kontur.Contest/Program.cs
+++ Kontur.Contest/Program.cs
@@ -1,41 +1,131 @@
 ﻿namespace Kontur.Contest
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var k = int.Parse(Console.ReadLine()!);
-            var numbers = new List<string>();
-
-            for (int i = 0; i < k; i++)
+            int n = int.Parse(Console.ReadLine()!);
+            (long c, long m)[] sanctuaries = new (long, long)[n];
+            for (int i = 0; i < n; i++)
             {
-                numbers.Add(Console.ReadLine()!);
+                string[] input = Console.ReadLine()!.Split();
+                long cVal = long.Parse(input[0]);
+                long mVal = long.Parse(input[1]);
+                sanctuaries[i] = (cVal, mVal);
             }
 
-            string result = GetStrLargestNumber(numbers);
-            Console.WriteLine(result);
+            long[] allC = sanctuaries.Select(s => s.c).ToArray();
+            Array.Sort(allC);
+            long[] sufSum = new long[allC.Length + 1];
+            for (int i = allC.Length - 1; i >= 0; i--)
+            {
+                sufSum[i] = sufSum[i + 1] + allC[i];
+            }
 
+            long bestCost = long.MaxValue;
+            int bestIndex = -1;
+            long bestT = 0;
 
+            for (int i = 0; i < n; i++)
+            {
+                if (sanctuaries[i].m == -1) continue;
+
+                long c_i = sanctuaries[i].c;
+                long T_max = FindTMax(c_i, allC, sufSum);
+
+                long cost;
+                if (T_max == c_i)
+                {
+                    long s_val = S(c_i, allC, sufSum);
+                    cost = sanctuaries[i].m + s_val - 1;
+                }
+                else
+                {
+                    long s_val = S(T_max, allC, sufSum);
+                    cost = sanctuaries[i].m + s_val;
+                }
+
+                if (cost < bestCost)
+                {
+                    bestCost = cost;
+                    bestIndex = i;
+                    bestT = T_max;
+                }
+            }
+
+            Console.WriteLine(bestCost);
+            Console.WriteLine(bestIndex + 1);
+
+            long[] newC = new long[n];
+            for (int j = 0; j < n; j++)
+            {
+                if (j == bestIndex)
+                {
+                    if (bestT == sanctuaries[j].c)
+                    {
+                        newC[j] = sanctuaries[j].c + S(bestT, allC, sufSum) - 1;
+                    }
+                    else
+                    {
+                        newC[j] = sanctuaries[j].c + S(bestT, allC, sufSum);
+                    }
+                }
+                else
+                {
+                    newC[j] = Math.Min(sanctuaries[j].c, bestT - 1);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", newC));
         }
 
-        static string GetStrLargestNumber(List<string> numbers)
+        private static long FindTMax(long c_i, long[] allC, long[] sufSum)
         {
-            numbers.Sort((a, b) =>
+            long L = c_i;
+            long R = allC[allC.Length - 1] + 1;
+            long T_max = c_i;
+            while (L <= R)
             {
-                string ab = a + b;
-                string ba = b + a;
-                return ba.CompareTo(ab); // Обратный порядок для получения убывающей сортировки
-            });
-
-            string result = string.Concat(numbers);
-
-            // Проверка на случай, если все числа нули
-            if (result.Length > 0 && result[0] == '0')
-            {
-                return "0";
+                long mid = (L + R) / 2;
+                long s_val = S(mid, allC, sufSum);
+                if (c_i + s_val >= mid)
+                {
+                    T_max = mid;
+                    L = mid + 1;
+                }
+                else
+                {
+                    R = mid - 1;
+                }
             }
+            return T_max;
+        }
 
-            return result;
+        private static long S(long T, long[] allC, long[] sufSum)
+        {
+            int k = LowerBound(allC, T);
+            int n = allC.Length;
+            if (k == n) return 0;
+            return sufSum[k] - (n - k) * (T - 1);
+        }
+
+        private static int LowerBound(long[] arr, long T)
+        {
+            int low = 0;
+            int high = arr.Length;
+            while (low < high)
+            {
+                int mid = (low + high) / 2;
+                if (arr[mid] < T)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid;
+                }
+            }
+            return low;
         }
     }
 }
