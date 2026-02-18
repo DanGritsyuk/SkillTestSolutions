5dca74582a16d2fd389f8c3b706a20e4e4252370
DanGritsyuk <denis@gritsyuk.ru>
2026-02-18 22:52:10 +0300

    End.

--- Kontur.Contest/Program.cs
+++ Kontur.Contest/Program.cs
@@ -4,69 +4,113 @@
     {
         static void Main()
         {
-            int sanctuaryCount = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine()!);
+            int[] c = new int[n];
+            int[] m = new int[n];
 
-            int[] cats = new int[sanctuaryCount];
+            const int MAX = 110;
+            int Vmax = 0;
 
-            int bestPriority = int.MinValue;
-            int chosenSanctuary = 0;
-            int indexOfMaxCats = -1;
-            int totalCost = 0;
+            int[] freqAll = new int[MAX + 1];
 
-            for (int i = 0; i < sanctuaryCount; i++)
+            int totalCats = 0;
+            for (int i = 0; i < n; i++)
             {
-                var data = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
-                int catCount = data[0];
-                int bribeCost = data[1];
-                cats[i] = catCount;
+                var parts = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+                c[i] = parts[0];
+                m[i] = parts[1];
+                freqAll[c[i]]++;
+                totalCats += c[i];
+                if (c[i] > Vmax) Vmax = c[i];
+            }
 
-                if (bribeCost >= 0)
+            int bestCost = int.MaxValue;
+            int bestIndex = -1;
+            int[] bestFinal = null!;
+
+            int[] prefCountAll = new int[MAX + 2];
+            int[] prefSumAll = new int[MAX + 2];
+
+            prefCountAll[Vmax + 1] = 0;
+            prefSumAll[Vmax + 1] = 0;
+            for (int v = Vmax; v >= 0; v--)
+            {
+                prefCountAll[v] = prefCountAll[v + 1] + freqAll[v];
+                prefSumAll[v] = prefSumAll[v + 1] + freqAll[v] * v;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (m[i] == -1) continue;
+
+                int ci = c[i];
+                int sumOthers = totalCats - ci;
+
+                int Tmax = ci + sumOthers;
+
+                int chosenT = -1;
+                for (int T = ci; T <= Tmax; T++)
                 {
-                    int currentPriority = catCount - bribeCost;
-
-                    if (currentPriority > bestPriority)
+                    int prefCount = (T <= Vmax) ? prefCountAll[T] : 0;
+                    int prefSum = (T <= Vmax) ? prefSumAll[T] : 0;
+                    if (ci >= T)
                     {
-                        bestPriority = currentPriority;
-                        totalCost = bribeCost;
-
-                        if (indexOfMaxCats > -1 && cats[chosenSanctuary] > cats[indexOfMaxCats])
-                        {
-                            indexOfMaxCats = chosenSanctuary;
-                        }
-
-                        chosenSanctuary = i;
+                        prefCount -= 1;
+                        prefSum -= ci;
                     }
-                    else
+
+                    int R = prefSum - (T - 1) * prefCount;
+
+                    int t = T - ci;
+                    if (t >= R)
                     {
-                        if (indexOfMaxCats < 0 || cats[i] > cats[indexOfMaxCats])
-                        {
-                            indexOfMaxCats = i;
-                        }
+                        chosenT = T;
+                        break;
                     }
                 }
-                else
+
+                if (chosenT == -1)
                 {
-                    if (indexOfMaxCats < 0 || cats[i] > cats[indexOfMaxCats])
+                    continue;
+                }
+
+                int tNeeded = chosenT - ci;
+                int totalCost = m[i] + tNeeded;
+                if (totalCost < bestCost)
+                {
+                    bestCost = totalCost;
+                    bestIndex = i;
+
+                    int[] final = new int[n];
+                    Array.Copy(c, final, n);
+
+                    int rem = tNeeded;
+
+                    var pairs = new List<(int val, int idx)>();
+                    for (int j = 0; j < n; j++)
                     {
-                        indexOfMaxCats = i;
+                        if (j == i) continue;
+                        pairs.Add((c[j], j));
                     }
+                    pairs.Sort((a, b) => b.val.CompareTo(a.val));
+
+                    for (int p = 0; p < pairs.Count && rem > 0; p++)
+                    {
+                        var (val, idx) = pairs[p];
+                        int take = Math.Min(val, rem);
+                        final[idx] = val - take;
+                        rem -= take;
+                    }
+
+                    final[i] = ci + tNeeded;
+
+                    bestFinal = final;
                 }
             }
 
-            if (indexOfMaxCats > -1 && cats[chosenSanctuary] >= cats[indexOfMaxCats])
-            {
-                int initialCats = cats[chosenSanctuary];
-                int combinedCats = cats[chosenSanctuary] + cats[indexOfMaxCats];
-
-                cats[chosenSanctuary] = combinedCats / 2 + 1;
-                cats[indexOfMaxCats] = combinedCats - cats[chosenSanctuary];
-
-                totalCost += cats[chosenSanctuary] - initialCats;
-            }
-
-            Console.WriteLine(totalCost);
-            Console.WriteLine(chosenSanctuary + 1);
-            Console.WriteLine(string.Join(" ", cats));
+            Console.WriteLine(bestCost);
+            Console.WriteLine(bestIndex + 1);
+            Console.WriteLine(string.Join(" ", bestFinal));
         }
     }
 }
